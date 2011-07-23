b1f3142c2f5f01963050193f6b8ce8e602e8fecd
Tom Adams <tom@holizz.com>
2011-07-23 12:56:53 -0400

    Toggling works

--- js/script.js
+++ js/script.js
@@ -8,4 +8,19 @@ jQuery(function($){
     $('#board').append('<tr></tr>');
   for (var i = 0; i < size; i++)
     $('#board tr').append('<td><button class="light on"></button></td>');
+
+  $.fn.toggleOnOff = function(){
+    $(this).toggleClass('on').toggleClass('off');
+  };
+
+  $('.light').click(function(){
+    var n = $(this).parent().prevAll().length + 1;
+    console.log(n);
+
+    $(this).toggleOnOff();
+    $(this).parent().prev().find('button').toggleOnOff();
+    $(this).parent().next().find('button').toggleOnOff();
+    $(this).parents('tr').prev().find('td:nth-child('+n+') button').toggleOnOff();
+    $(this).parents('tr').next().find('td:nth-child('+n+') button').toggleOnOff();
+  });
 });
