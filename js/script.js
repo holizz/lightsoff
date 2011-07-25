ca8ebad2ef030c7eb125b9ec113f8d1d488e2801
Tom Adams <tom@holizz.com>
2011-07-25 00:25:40 -0400

    OOPify

--- js/script.js
+++ js/script.js
@@ -2,53 +2,71 @@
 
 */
 
-jQuery(function($){
 
-  var loadGame = function(game) {
-    $('#board .light').removeClass('off').addClass('on');
-    game = game.replace(/\|/g, '');
+var LightsOff = Class.$extend({
+  __init__: function(data) {
+    this.board = $(data.board);
+    this.moveCounter = $(data.moveCounter);
+
+    this.defineFunctions();
+    this.createBoard();
+    this.mapHandlers();
+
+    //this.loadLevel('00000|00110|01110|10100|01000');
+    this.loadLevel('00000|00100|01110|10100|11000');
+  },
+
+  defineFunctions: function() {
+    $.fn.toggleOnOff = function(){
+      if ($(this).is('.light'))
+        $(this).toggleClass('on').toggleClass('off');
+      else if (this.length > 0)
+        $(this).find('.light').toggleOnOff();
+    };
+  },
+
+  createBoard: function() {
+    var size = 5;
+    for (var i = 0; i < size; i++)
+      this.board.append('<tr></tr>');
+    for (var i = 0; i < size; i++)
+      $('tr', this.board).append('<td><button class="light on"></button></td>');
+  },
+
+  mapHandlers: function() {
+    var thus = this;
+
+    $('.light').click(function(){
+      var n = $(this).parent().prevAll().length + 1;
+
+      $(this).toggleOnOff();
+      $(this).parent().prev().toggleOnOff();
+      $(this).parent().next().toggleOnOff();
+      $(this).parents('tr').prev().find('td:nth-child('+n+')').toggleOnOff();
+      $(this).parents('tr').next().find('td:nth-child('+n+')').toggleOnOff();
+
+      thus.moveCounter.val(parseInt(thus.moveCounter.val()) + 1);
+
+      return false;
+    });
+  },
+
+  loadLevel: function(level) {
+    $('.light', this.board).removeClass('off').addClass('on');
+    level = level.replace(/\|/g, '');
 
     var i = 0;
-    $('#board .light').each(function(){
-      if (game[i]==='0')
+    $('.light', this.board).each(function(){
+      if (level[i]==='0')
         $(this).toggleOnOff();
       i++;
     });
   }
+});
 
-  $.fn.toggleOnOff = function(){
-    if ($(this).is('.light'))
-      $(this).toggleClass('on').toggleClass('off');
-    else if (this.length > 0)
-      $(this).find('.light').toggleOnOff();
-  };
-
-  // Create board
-
-  var size = 5;
-  for (var i = 0; i < size; i++)
-    $('#board').append('<tr></tr>');
-  for (var i = 0; i < size; i++)
-    $('#board tr').append('<td><button class="light on"></button></td>');
-
-  // Allow buttons to be pressed
-
-  $('.light').click(function(){
-    var n = $(this).parent().prevAll().length + 1;
-
-    $(this).toggleOnOff();
-    $(this).parent().prev().toggleOnOff();
-    $(this).parent().next().toggleOnOff();
-    $(this).parents('tr').prev().find('td:nth-child('+n+')').toggleOnOff();
-    $(this).parents('tr').next().find('td:nth-child('+n+')').toggleOnOff();
-
-    $('#move-counter').val(parseInt($('#move-counter').val()) + 1);
-
-    return false;
+jQuery(function($){
+  new LightsOff({
+    board: '#board',
+    moveCounter: '#move-counter'
   });
-
-  // Set up game
-
-  //loadGame('00000|00110|01110|10100|01000');
-  loadGame('00000|00100|01110|10100|11000');
 });
